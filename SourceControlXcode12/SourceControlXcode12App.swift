617a9837ef1c30cd720f710adb2a1bd3f2c0b554
Kathy Anderson <anderson.kathy@gmail.com>
2021-02-13 22:21:19 -0700

    Initial Commit

---
+++ SourceControlXcode12/SourceControlXcode12App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlXcode12App.swift
+//  SourceControlXcode12
+//
+//  Created by Kathy Anderson on 2/13/21.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlXcode12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
